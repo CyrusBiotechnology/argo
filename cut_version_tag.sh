7e7ea33daa0143858b9b4d78353f5f4f6fcc3d6d
Sam DeLuca <sam@cyrusbio.com>
2019-05-20 11:57:03 -0400

    updating cut-version

--- cut_version_tag.sh
+++ cut_version_tag.sh
@@ -6,11 +6,13 @@ echo "pulling latest changes"
 git pull
 
 version=`cat VERSION`
+environment=$1
+full_version="$version-$environment"
 
 echo "current git HEAD is \"$(git log --oneline |head -1)\""
-read -p "Would you like to create and push the tag ${version} at the current head of the master branch? (y/n)" proceed
+read -p "Would you like to create and push the tag ${full_version} at the current head of the master branch? (y/n)" proceed
 
 if [[ ${proceed} == "y" ]]; then
-    git tag ${version}
+    git tag ${full_version}
     git push --tags
 fi
