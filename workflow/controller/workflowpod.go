aa44b3a420c2aa26016c6fd1fe083db4634ad6a6
Mary Larrousse <larroussemary@gmail.com>
2019-06-14 15:20:27 -0400

    k

--- workflow/controller/workflowpod.go
+++ workflow/controller/workflowpod.go
@@ -81,7 +81,7 @@ func (woc *wfOperationCtx) createWorkflowPod(nodeName string, mainCtr apiv1.Cont
 			Labels: map[string]string{
 				common.LabelKeyWorkflow:  woc.wf.ObjectMeta.Name, // Allows filtering by pods related to specific workflow
 				common.LabelKeyCompleted: "false",                // Allows filtering by incomplete workflow pods
-				common.LabelKeyWorkflowType: woc.worflow_name     // Allows filtering by workflow type for VPA
+				common.LabelKeyWorkflowType: woc.workflow_name     // Allows filtering by workflow type for VPA
 				common.LabelKeyTemplate: woc.template             // Allows filtering by workflow template for VPA
 
 			},
