3e6ad6d64088b80a8eba016aea7bca0d59aade76
Mary Larrousse <mary@cyrusbio.com>
2019-06-19 13:57:10 -0400

    remove invalid characters

--- workflow/controller/workflowpod.go
+++ workflow/controller/workflowpod.go
@@ -81,7 +81,7 @@ func (woc *wfOperationCtx) createWorkflowPod(nodeName string, mainCtr apiv1.Cont
 			Labels: map[string]string{
 				common.LabelKeyWorkflow:  woc.wf.ObjectMeta.Name, 				// Allows filtering by pods related to specific workflow
 				common.LabelKeyCompleted: "false",                				// Allows filtering by incomplete workflow pods
-				common.LabelKeyWorkflowType: woc.wf.ObjectMeta.GenerateName,  	// Allows filtering by workflow type for VPA
+				common.LabelKeyWorkflowType: strings.Replace(woc.wf.ObjectMeta.GenerateName, "-", "", -1),  	// Allows filtering by workflow type for VPA
 				common.LabelKeyTemplate: tmpl.Name,                				// Allows filtering by workflow template for VPA
 
 			},
