b5bef63730f9b8f01092a2588c4505196b0c80d0
Sam DeLuca <sam@cyrusbio.com>
2019-08-09 13:05:06 -0400

    rewriting gcs client to handle directories

--- workflow/artifacts/gcs/gcs.go
+++ workflow/artifacts/gcs/gcs.go
@@ -4,24 +4,29 @@ import (
 	"cloud.google.com/go/storage"
 	"context"
 	"errors"
+	"github.com/argoproj/pkg/file"
 	argoErrors "github.com/cyrusbiotechnology/argo/errors"
 	wfv1 "github.com/cyrusbiotechnology/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/cyrusbiotechnology/argo/util"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
 	"io"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type GCSArtifactDriver struct {
-	Context       context.Context
 	CredsJSONData []byte
 }
 
-func (gcsDriver *GCSArtifactDriver) newGcsClient() (client *storage.Client, err error) {
-	gcsDriver.Context = context.Background()
+func (gcsDriver *GCSArtifactDriver) newGcsClient() (client GCSClient, err error) {
 
-	client, err = storage.NewClient(gcsDriver.Context, option.WithCredentialsJSON(gcsDriver.CredsJSONData))
+	client, err = NewGCSClient(GCSClientOpts{CredsJSONData: gcsDriver.CredsJSONData})
+
 	if err != nil {
 		return nil, argoErrors.InternalWrapError(err)
 	}
@@ -33,20 +38,6 @@ func (gcsDriver *GCSArtifactDriver) saveToFile(inputArtifact *wfv1.Artifact, fil
 
 	log.Infof("Loading from GCS (gs://%s/%s) to %s",
 		inputArtifact.GCS.Bucket, inputArtifact.GCS.Key, filePath)
-
-	stat, err := os.Stat(filePath)
-	if err != nil && !os.IsNotExist(err) {
-		return err
-	}
-
-	if stat != nil && stat.IsDir() {
-		return errors.New("output artifact path is a directory")
-	}
-
-	outputFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
 
 	gcsClient, err := gcsDriver.newGcsClient()
 	if err != nil {
@@ -74,6 +65,7 @@ func (gcsDriver *GCSArtifactDriver) saveToFile(inputArtifact *wfv1.Artifact, fil
 	return nil
 }
 
+
 func (gcsDriver *GCSArtifactDriver) saveToGCS(outputArtifact *wfv1.Artifact, filePath string) error {
 
 	log.Infof("Saving to GCS (gs://%s/%s)",
@@ -94,9 +86,12 @@ func (gcsDriver *GCSArtifactDriver) saveToGCS(outputArtifact *wfv1.Artifact, fil
 		return err
 	}
 
-	if stat.IsDir() {
-		return errors.New("only single files can be saved to GCS, not entire directories")
-	}
+	//if stat.IsDir() {
+	//	for putTask := range generatePutTasks(outputArtifact.GCS.Bucket, outputArtifact.GCS.Key, filePath) {
+	//		err :=
+	//	}
+	//	return errors.New("only single files can be saved to GCS, not entire directories")
+	//}
 
 	defer util.Close(inputFile)
 
@@ -119,12 +114,72 @@ func (gcsDriver *GCSArtifactDriver) saveToGCS(outputArtifact *wfv1.Artifact, fil
 
 func (gcsDriver *GCSArtifactDriver) Load(inputArtifact *wfv1.Artifact, path string) error {
 
-	err := gcsDriver.saveToFile(inputArtifact, path)
+	bucketName := inputArtifact.GCS.Bucket
+	key := inputArtifact.GCS.Key
+
+	err := wait.ExponentialBackoff(wait.Backoff{Duration: time.Second * 2, Factor: 2.0, Steps: 5, Jitter: 0.1},
+		func() (bool, error) {
+			log.Infof("GCS Load path: %s, key: %s", path, key)
+			gcsClient, err := gcsDriver.newGcsClient()
+			if err != nil {
+				log.Warnf("Failed to create new GCS client: %v", err)
+				return false, nil
+			}
+
+			isDir, err := gcsClient.IsDirectory(bucketName, key)
+			if err != nil {
+				log.Warnf("Failed to test if %s is a directory: %v", bucketName, err)
+				return false, nil
+			}
+
+			if isDir {
+				if err = gcsClient.GetDirectory(bucketName, key, path)
+				err != nil {
+					log.Warnf("Failed get directory: %v", err)
+					return false, nil
+				}
+			} else {
+				err := gcsClient.GetFile(bucketName, key, path)
+				if  err != nil {
+					return false, nil
+				}
+			}
+
+			return true, nil
+		})
+
 	return err
 }
 
 func (gcsDriver *GCSArtifactDriver) Save(path string, outputArtifact *wfv1.Artifact) error {
+	bucketName := outputArtifact.GCS.Bucket
+	key := outputArtifact.GCS.Key
 
-	err := gcsDriver.saveToGCS(outputArtifact, path)
+	err := wait.ExponentialBackoff(wait.Backoff{Duration: time.Second * 2, Factor: 2.0, Steps: 5, Jitter: 0.1},
+		func() (bool, error) {
+			log.Infof("S3 Save path: %s, key: %s", path, key)
+			gcsClient, err := gcsDriver.newGcsClient()
+			if err != nil {
+				log.Warnf("Failed to create new S3 client: %v", err)
+				return false, nil
+			}
+			isDir, err := file.IsDirectory(path)
+			if err != nil {
+				log.Warnf("Failed to test if %s is a directory: %v", path, err)
+				return false, nil
+			}
+			if isDir {
+				if err = gcsClient.PutDirectory(bucketName, key, path); err != nil {
+					log.Warnf("Failed to put directory: %v", err)
+					return false, nil
+				}
+			} else {
+				if err = gcsClient.PutFile(bucketName, key, path); err != nil {
+					log.Warnf("Failed to put file: %v", err)
+					return false, nil
+				}
+			}
+			return true, nil
+		})
 	return err
 }
