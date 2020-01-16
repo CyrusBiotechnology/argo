3b582d8517008c7cff995e447307aafa3f81d6b8
Sam DeLuca <sam@cyrusbio.com>
2020-01-16 17:17:35 -0500

    fixing a bug in workflow loading

--- workflow/controller/config_controller.go
+++ workflow/controller/config_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -21,12 +22,28 @@ import (
 
 // ResyncConfig reloads the controller config from the configmap
 func (wfc *WorkflowController) ResyncConfig() error {
-	cmClient := wfc.kubeclientset.CoreV1().ConfigMaps(wfc.namespace)
-	cm, err := cmClient.Get(wfc.configMap, metav1.GetOptions{})
-	if err != nil {
-		return errors.InternalWrapError(err)
+
+	if wfc.configFile != "" {
+		log.Infof("Loading configfile from %s", wfc.configFile)
+		return wfc.updateConfigFromFile(wfc.configFile)
+	} else {
+		cmClient := wfc.kubeclientset.CoreV1().ConfigMaps(wfc.namespace)
+		cm, err := cmClient.Get(wfc.configMap, metav1.GetOptions{})
+		if err != nil {
+			return errors.InternalWrapError(err)
+		}
+		return wfc.updateConfig(cm)
 	}
-	return wfc.updateConfig(cm)
+}
+
+func (wfc *WorkflowController) updateConfigFromFile(filePath string) error {
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Errorf("Error reading config file %s", filePath)
+		return err
+	}
+	return wfc.updateConfig(string(fileData))
+
 }
 
 func (wfc *WorkflowController) updateConfig(cm *apiv1.ConfigMap) error {
